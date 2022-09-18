426cbb2f6af3322d440cf8129e5b6745bb0882e8
sohrab09 <sohrab@nassagroup.org>
2022-09-18 12:37:19 +0600

    NCash page content added

--- src/screens/EarlyLeave.js
+++ src/screens/EarlyLeave.js
@@ -1,19 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Pressable, Alert } from 'react-native'
 import React from 'react'
-import SelectList from 'react-native-dropdown-select-list'
+import RadioButtonRN from 'radio-buttons-react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createMaterialTopTabNavigator();
 
 const EarlyLeave = () => {
 
-
+  const data = [
+    {
+      label: '1st Half'
+    },
+    {
+      label: '2nd Half'
+    }
+  ];
 
 
   function Request() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-         <Text>Request!</Text>
+      <View style={{ marginTop: '10%' }}>
+        <View>
+          <RadioButtonRN
+            data={data}
+            // selectedBtn={(e) => console.log(e)}
+            box={false}
+            icon={
+              <Icon
+                name="check-circle"
+                size={25}
+                color="#2c9dd1"
+              />
+            }
+          />
+        </View>
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder="Remarks"
+            placeholderTextColor="#ccc"
+          />
+        </View>
+        <View>
+          <Text>In Time</Text>
+          <TextInput
+            style={styles.input2}
+            placeholder="In Time"
+            placeholderTextColor="#ccc"
+          />
+        </View>
+
+        <View>
+          <Pressable
+            onPress={() => Alert.alert('Sorry', 'API is not ready', [
+              { text: 'Understood' }
+            ])}
+            style={styles.button}
+          >
+            <Text style={{ color: '#ccc', textTransform: 'uppercase', fontWeight: 'bold' }}>Confirm</Text>
+          </Pressable>
+        </View>
       </View>
     );
   }
@@ -46,4 +93,34 @@ const EarlyLeave = () => {
 
 export default EarlyLeave
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    margin: "5%",
+    padding: "5%",
+    borderColor: '#ccc',
+    borderRadius: 10,
+    height: 100,
+  },
+  input2: {
+    borderWidth: 1,
+    margin: "5%",
+    padding: "5%",
+    borderColor: '#ccc',
+    borderRadius: 10,
+    // height: 50,
+
+  },
+
+  button: {
+    backgroundColor: "#fff",
+    padding: 10,
+    margin: 15,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "50%",
+    marginLeft: '25%',
+    alignItems: "center",
+  }
+})
--- src/screens/NCash.js
+++ src/screens/NCash.js
@@ -1,10 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const NCash = () => {
+
+  const [taka, setTaka] = useState(0)
+
   return (
     <View>
-      <Text>NCash</Text>
+      <Text style={{
+        color: taka > 0 ? 'green' : 'red',
+        fontSize: taka > 0 ? 30 : 20,
+        fontWeight: taka < 0 ? 'bold' : 'normal',
+        textAlign: 'center',
+        marginTop: 100,
+      }}>
+        {
+          taka <= 0 ? 'Please Recharge' : taka
+        }
+      </Text>
+      <Text style={{
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+      }}>
+        Your current balance.
+      </Text>
     </View>
   )
 }
